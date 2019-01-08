43aa4748afbce30ea20866ee6ab40b744abfb43e
blakereeves19 <blake.reeves19@gmail.com>
2019-01-07 19:06:34 -0600

    Added keypress functions

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+let i = 0;
+let currentSentence = sentences[i];
+let letterPosition = 0;
+let currentLetter = currentSentence.substring(letterPosition, letterPosition + 1);
+
 $('#keyboard-upper-container').hide();
 
 $(document).keydown(function (e) {
@@ -26,4 +32,32 @@ $(document).keypress(function (e) {
     $(document).keyup(function (e) {
         $(key).css('background', 'whitesmoke');
     });
+});
+
+$('#sentence').text(currentSentence);
+$('#target-letter').text(currentLetter);
+
+$(document).keypress(function (e) {
+
+   if (e.which === sentences[i].charCodeAt(letterPosition)) {
+
+        letterPosition++;
+        currentLetter = currentSentence.substring(letterPosition, letterPosition + 1);
+        $('#target-letter').text(currentLetter);
+    
+        if (letterPosition === currentSentence.length) {
+    
+            i++;
+    
+        } else {
+            
+            currentSentence = sentences[i];
+            $('#sentence').text(currentSentence);
+            letterPosition = 0;
+            currentLetter = currentSentence.substring(letterPosition, letterPosition + 1);
+            $('#target-letter').text(currentLetter);
+            console.log(letterPosition);
+    
+        }
+    }
 });
